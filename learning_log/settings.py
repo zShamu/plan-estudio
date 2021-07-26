6274e26fe2f40e3bb68691828583054af2b72f65
Shamuel Vargas <63377518+zShamu@users.noreply.github.com>
2021-07-25 21:06:42 -0500

    Security changes

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -19,11 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7wyhmp%he@w&#nsjxe_rj9%3fxiui+)qm20m7fx0i(_oa*p0f3'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
